9bbefa837b3dea5b84b9fbd417dd44cd07c711e2
Shane Khalid Lim <78730-22216550@SERVER2.DOMAIN>
2024-03-02 13:27:53 +0800

    Shane Activity 2

---
+++ src/Student.jsx
@@ -0,0 +1,13 @@
+function Food(){
+    const food1 = "Orange";
+    const studName = "Shane";
+    const studEmail = "shanekhalidlim@gmail.com";
+    return(
+        <ul>
+            <li></li>
+            <li>{studName.toUpperCase()}</li>
+            <li>{studEmail.toLowerCase()}</li>
+        </ul>
+    );
+}
+export default Food
